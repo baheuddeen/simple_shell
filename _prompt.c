bc560436073477df42ea90bff76b35b03164b910
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-04-25 13:06:22 +0200

    impelent _getline

--- _prompt.c
+++ _prompt.c
@@ -1,12 +1,11 @@
 #include "shell.h"
 
-
 /**
  * _prompt - prompt user to enter a command
  * @flag_exit: flag to exit
  * Return: the buffer
  */
-char *_prompt(int *flag_exit)
+char *_prompt()
 {
 	char *buffer = NULL;
 	size_t bufsize = 256;
@@ -17,11 +16,11 @@ char *_prompt(int *flag_exit)
 	{
 		perror("Unable to allocate buffer");
 	}
-
 	if (!isatty(STDIN_FILENO))
 	{
-		characters = getline(&buffer, &bufsize, stdin);
-		*flag_exit = 1;
+		characters = _getline(&buffer, &bufsize, STDIN_FILENO);
+		if (!characters)
+			exit(1);
 	}
 	else
 	{
@@ -43,7 +42,6 @@ char *_prompt(int *flag_exit)
 	}
 
 	return (buffer);
-	return (0);
 }
 
 /**
@@ -63,4 +61,37 @@ int _get_length(char *buffer)
 	}
 	length++;
 	return (length);
+}
+
+/**
+ * _getline - get the line
+ * @buffer: the buffer
+ * @bufsize: the buffer size
+ * @fd: the file descriptor
+ * Return: the length
+ */
+int _getline(char **buffer, size_t *bufsize, int fd)
+{
+	size_t i = 0;
+	char c;
+
+	while (read(fd, &c, 1) > 0)
+	{
+		if (i >= *bufsize)
+		{
+			*bufsize += 256;
+			*buffer = realloc(*buffer, *bufsize);
+			if (*buffer == NULL)
+			{
+				perror("Unable to reallocate buffer");
+				exit(1);
+			}
+		}
+		(*buffer)[i] = c;
+		i++;
+		if (c == '\n')
+			break;
+	}
+	(*buffer)[i] = '\0';
+	return (i);
 }
