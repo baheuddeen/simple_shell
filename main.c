2c109d1ea7720557aac05fdf7ae08365cc826435
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-04-26 23:28:33 +0200

    hopeless

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 int execute(char **args, char **front);
 
 /**
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
+ */
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
+}
+
+/**
  * execute - Executes a command in a child process.
  * @args: An array of arguments.
  * @front: A double pointer to the beginning of args.
